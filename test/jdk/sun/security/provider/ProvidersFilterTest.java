e553cfd15d1d5b3f67db5b15e843d68302aa8b66
Martin Balao <mbalao@redhat.com>
2024-02-08 22:04:51 -0500

    Minor changes to align with the JEP.
    Co-authored-by: Francisco Ferrari Bihurriet <fferrari@redhat.com>
    Co-authored-by: Martin Balao <mbalao@redhat.com>

--- test/jdk/sun/security/provider/ProvidersFilterTest.java
+++ test/jdk/sun/security/provider/ProvidersFilterTest.java
@@ -803,6 +803,8 @@ public final class ProvidersFilterTest {
                 "PBEWithHmacSHA512/256AndAES_128/CBC/PKCS5Padding");
         t.addNotExpectedService("SunJCE", "Cipher", "AES");
         t.addNotExpectedService("SunJCE", "Cipher", "AES//");
+        t.addNotExpectedService("SunJCE", "Cipher", KnownOIDs.AES.value() +
+                "//");
         t.addNotExpectedService("SunJCE", "Cipher", KnownOIDs.AES.value());
         t.addNotExpectedService("SunJCE", "Cipher", "AES/CBC/NoPadding");
         t.addNotExpectedService("SunJCE", "Cipher",
